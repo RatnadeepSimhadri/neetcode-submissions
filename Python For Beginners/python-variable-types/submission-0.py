ee90e069c20714347ceec82b4dea6c79b8a2d945
Ratna Deep Simhadri <ratnadeep.ms@outlook.com>
2026-05-30 10:43:41 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,13 @@
+integer_type = 25
+float_type = 98.6 
+bool_type = False
+string_type = "Ratnadeep"
+list_type = ["Apple", "Banana", "Strawberry"]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
