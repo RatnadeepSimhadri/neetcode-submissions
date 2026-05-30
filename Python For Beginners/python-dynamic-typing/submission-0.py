f3480438a22b729b74b38754bebe8af0d3995dbb
Ratna Deep Simhadri <ratnadeep.ms@outlook.com>
2026-05-30 10:44:51 -0700

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.5
+print(type(variable))
+variable = False
+print(type(variable))
+variable = "Simhadri"
+print(type(variable))
+variable = [1,2,3,4]
+print(type(variable))
