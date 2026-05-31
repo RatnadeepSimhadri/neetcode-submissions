88f66240c7fddb1d690503877ce8cb53f7ec6726
Ratna Deep Simhadri <ratnadeep.ms@outlook.com>
2026-05-31 01:30:18 -0700

    Add: python-string-looping-shorthand - submission-0

---
+++ Python For Beginners/python-string-looping-shorthand/submission-0.py
@@ -0,0 +1,12 @@
+def print_string_characters(word1: str, word2: str) -> None:
+    for _char in word1:
+        print(_char)
+
+    for _char in word2:
+        print(_char)
+
+
+
+
+# do not modify below this line
+print_string_characters("Hello, World!", "Good Job!")
