38776db41bca87f2f0386386ea972d47a366bb9c
Ratna Deep Simhadri <ratnadeep.ms@outlook.com>
2026-06-13 17:12:47 -0700

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,21 @@
+class MinStack:
+
+    def __init__(self):
+       self.og_stack = []
+       self.min_stack = [] 
+
+    def push(self, val: int) -> None:
+        self.og_stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        popped = self.og_stack.pop()
+        if popped == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.og_stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
