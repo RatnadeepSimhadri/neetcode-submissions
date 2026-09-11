342ecda3efea3d3a0aecf0df8c44db9019db28c0
Ratna Deep Simhadri <ratnadeep.ms@outlook.com>
2026-09-10 22:51:41 -0700

    Add: python-read-input-practice - submission-1

---
+++ Python For Beginners/python-read-input-practice/submission-1.py
@@ -0,0 +1,12 @@
+def add_two_numbers() -> int:
+    line = input()
+    num_strings = line.split(",")
+    return int(num_strings[0]) + int(num_strings[1])
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
